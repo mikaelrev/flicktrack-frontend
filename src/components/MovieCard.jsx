4ebd6efa867656321362cb61ab6741e406202f04
mikaelrev <79143791+mikaelrev@users.noreply.github.com>
2025-02-13 15:12:45 +0100

    fixes styling on moviecard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,12 +4,12 @@ function MovieCard({ movie }) {
 	return (
 		<Link to={`/movies/${movie.id}`}>
 			<li key={movie.id}>
-				<h3>{movie.title}</h3>
 				<img
 					className="w-full h-auto rounded-md shadow-lg"
 					src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
 				/>
 			</li>
+			<h3>{movie.title}</h3>
 		</Link>
 	);
 }
