2f7536c736c007afeb57c4324804fdef7bc2d6af
mikaelrev <79143791+mikaelrev@users.noreply.github.com>
2025-02-18 14:38:44 +0100

    adds checked movies and lists to profile page, and adds some styling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -37,14 +37,20 @@ function Profile() {
 	return (
 		<div className="container mx-auto flex flex-col py-5">
 			<div className="w-full">
-				<h1 className="font-bold text-xl mb-4">{user.username}</h1>
+				<h1 className="text-3xl font-extrabold text-gray-700 mb-4">
+					{user.username}
+				</h1>
 				{user.profileImage === null ? (
 					<img
 						src="https://static.vecteezy.com/system/resources/thumbnails/005/129/844/small/profile-user-icon-isolated-on-white-background-eps10-free-vector.jpg"
 						alt="avatar"
 					/>
 				) : (
-					<img src={user.profileImage} alt={user.username} />
+					<img
+						className="h-48 w-48 object-none"
+						src={user.profileImage}
+						alt={user.username}
+					/>
 				)}
 				<p className="text-gray-700 mb-4">Bio: {user.bio}</p>
 				<p className="text-gray-700 mb-4">Quote: {user.quote}</p>
@@ -55,13 +61,45 @@ function Profile() {
 					Favorites: {user.favoriteMovies.length}
 				</p>
 			</div>
-			<div>
-				<p className="text-gray-700 mb-4">
-					Lists:{" "}
-					{user.lists.length === 0
-						? "No lists found"
-						: user.lists.map((list) => <span key={list._id}>{list.name}</span>)}
-				</p>
+			<hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+			<div className="bg-gray-100 p-3 rounded mb-4">
+				<h2 className="text-3xl font-extrabold text-gray-700 mb-4">
+					Checked movies:
+				</h2>
+				{user.checkedMovies.slice(0, 3).map((movie) => (
+					<div className="flex items-center gap-3" key={movie._id}>
+						<img
+							className="h-30 w-15 object-contain"
+							src={movie.posterUrl}
+							alt={`Poster for ${movie.title}`}
+						/>
+						<p>{movie.title}</p>
+					</div>
+				))}
+			</div>
+			<div className="bg-gray-100 p-3 rounded">
+				<h2 className="text-3xl font-extrabold text-gray-700 mb-4">Lists:</h2>
+				{user.lists.length === 0
+					? "No lists found"
+					: user.lists.map((list) => (
+							<div key={list._id}>
+								<h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+									{list.name}
+								</h3>
+								<ul>
+									{list.movies.map((movie) => (
+										<li className="flex items-center gap-3" key={movie._id}>
+											<img
+												className="h-30 w-15 object-contain"
+												src={movie.posterUrl}
+												alt={movie.title}
+											/>
+											<p>{movie.title}</p>
+										</li>
+									))}
+								</ul>
+							</div>
+					  ))}
 			</div>
 		</div>
 	);
