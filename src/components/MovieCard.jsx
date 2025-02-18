6e6161e33db1e1c4ee4bc84a339eafbd94de3693
mikaelrev <79143791+mikaelrev@users.noreply.github.com>
2025-02-18 15:23:33 +0100

    displays movies in movies page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,11 +5,13 @@ function MovieCard({ movie }) {
 		<Link to={`/movies/${movie.id}`}>
 			<li key={movie.id}>
 				<img
-					className="w-full h-auto rounded-md shadow-lg"
+					className="w-full h-auto rounded-md shadow-lg mb-2"
 					src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
 				/>
 			</li>
-			<h3>{movie.title}</h3>
+			<h3 className="text-xl font-bold tracking-tight text-gray-700 dark:text-white">
+				{movie.title}
+			</h3>
 		</Link>
 	);
 }
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,4 +1,14 @@
-function Movies() {
-	return <div>Movies</div>;
+import MovieCard from "../components/MovieCard";
+
+function Movies({ movies }) {
+	return (
+		<div className="md:container md:mx-auto flex flex-col items-center justify-center p-5 mt-5">
+			<ul className="grid grid-cols-6 gap-5">
+				{movies.map((movie) => (
+					<MovieCard movie={movie} key={movie.id} />
+				))}
+			</ul>
+		</div>
+	);
 }
 export default Movies;
