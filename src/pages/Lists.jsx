f2987ba4ffd4d0f75ef17b6c53cb9955734abb42
mikaelrev <79143791+mikaelrev@users.noreply.github.com>
2025-02-18 15:44:42 +0100

    adds lists to list page

--- src/pages/Lists.jsx
+++ src/pages/Lists.jsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from "react";
+
 function Lists() {
-	return <div>Lists</div>;
+	const [lists, setLists] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:3000/lists")
+			.then((response) => response.json())
+			.then((data) => setLists(data.lists));
+	}, []);
+
+	console.log(lists);
+
+	return (
+		<div className="flex flex-col">
+			<ul className="flex flex-col gap-2 justify-center items-center">
+				{lists.length === 0
+					? "No lists found"
+					: lists.map((list) => (
+							<li key={list._id}>
+								<p>{list.name}</p>
+								{list.movies.length === 0
+									? ""
+									: list.movies.map((movie) => (
+											<div className="flex items-center gap-3" key={movie._id}>
+												<img
+													className="h-30 w-15 object-contain"
+													src={movie.posterUrl}
+												/>
+												<p>{movie.title}</p>
+											</div>
+									  ))}
+							</li>
+					  ))}
+			</ul>
+		</div>
+	);
 }
 export default Lists;
