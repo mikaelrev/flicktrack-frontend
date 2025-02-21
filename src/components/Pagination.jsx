4946f9ed5bdf24968bf474b4141382bf473410ee
mikaelrev <79143791+mikaelrev@users.noreply.github.com>
2025-02-21 12:28:44 +0100

    fetches movies in movies page, and adds pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,40 @@
+function Pagination({ goToPage, page, totalPages }) {
+	if (!totalPages || page <= 0) return null;
+
+	return (
+		<div className="flex justify-center items-center gap-2 mt-6">
+			<button
+				onClick={() => goToPage(page - 1)}
+				disabled={page === 1}
+				className="px-4 py-2 bg-gray-700 text-white rounded-md disabled:opacity-50"
+			>
+				Previous
+			</button>
+
+			{[...Array(5)].map((_, i) => {
+				const pageNumber = Math.max(1, page - 2) + i;
+				if (pageNumber > totalPages) return null;
+				return (
+					<button
+						key={pageNumber}
+						onClick={() => goToPage(pageNumber)}
+						className={`px-4 py-2 rounded-md ${
+							page === pageNumber ? "bg-blue-500 text-white" : "bg-gray-300"
+						}`}
+					>
+						{pageNumber}
+					</button>
+				);
+			})}
+
+			<button
+				onClick={() => goToPage(page + 1)}
+				disabled={page === totalPages}
+				className="px-4 py-2 bg-gray-700 text-white rounded-md disabled:opacity-50"
+			>
+				Next
+			</button>
+		</div>
+	);
+}
+export default Pagination;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,13 +1,43 @@
+import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
+import { useSearchParams } from "react-router-dom";
+import Pagination from "../components/Pagination";
 
-function Movies({ movies }) {
+const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+function Movies() {
+	const [movies, setMovies] = useState([]);
+	const [totalPages, setTotalPages] = useState(1);
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const page = parseInt(searchParams.get("page")) || 1;
+
+	const fetchMovies = async (page) => {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?page=${page}&api_key=${TMDB_API_KEY}`
+		);
+		const data = await res.json();
+		setMovies(data.results);
+		setTotalPages(data.total_pages > 500 ? 500 : data.total_pages);
+	};
+
+	useEffect(() => {
+		fetchMovies(page);
+	}, [page]);
+
+	const goToPage = (newPage) => {
+		setSearchParams({ page: newPage });
+	};
+
 	return (
 		<div className="md:container md:mx-auto flex flex-col items-center justify-center p-5 mt-5">
-			<ul className="grid grid-cols-6 gap-5">
+			<ul className="grid grid-cols-5 gap-5">
 				{movies.map((movie) => (
 					<MovieCard movie={movie} key={movie.id} />
 				))}
 			</ul>
+
+			<Pagination totalPages={totalPages} page={page} goToPage={goToPage} />
 		</div>
 	);
 }
