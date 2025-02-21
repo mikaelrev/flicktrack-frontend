75f8ee5b65578be9ede28a1259f3f6b720048a18
mikaelrev <79143791+mikaelrev@users.noreply.github.com>
2025-02-21 14:33:32 +0100

    removes movies data from ListItem component

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -7,18 +7,8 @@ function ListItem({ list }) {
 				<h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
 					{list.name}
 				</h3>
-				{list.movies.length === 0
-					? ""
-					: list.movies.map((movie) => (
-							<div className="flex p-2 items-center gap-3" key={movie._id}>
-								<img
-									className="h-30 w-15 object-contain"
-									src={movie.posterUrl}
-									alt={movie.title}
-								/>
-								<p>{movie.title}</p>
-							</div>
-					  ))}
+				<p>{`Movies: ${list.movies.length}`}</p>
+
 				<div>
 					{list.owner.username === undefined ? (
 						""
