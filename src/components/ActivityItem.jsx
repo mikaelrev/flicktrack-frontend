50f66f85ab5231c87da3b63336ef77d19ba3f606
mikaelrev <79143791+mikaelrev@users.noreply.github.com>
2025-03-05 11:44:49 +0100

    adds all activities to activityItem, not just checked and favorite

--- src/components/ActivityItem.jsx
+++ src/components/ActivityItem.jsx
@@ -1,8 +1,38 @@
 function ActivityItem({ activity }) {
-	return (
-		<div key={activity._id}>
-			{`${activity.user.username} added ${activity.targetMovie.title} to ${activity.activity}`}
-		</div>
-	);
+	const { user, activity: type, targetMovie, targetList, comment } = activity;
+
+	let message;
+	switch (type) {
+		case "checked":
+		case "favorite":
+			message = targetMovie
+				? `${user.username} added ${targetMovie.title} to ${type}`
+				: `${user.username} performed an action: ${type}`;
+			break;
+		case "added_to_list":
+			message =
+				targetMovie && targetList
+					? `${user.username} added ${targetMovie.title} to the list "${targetList.name}"`
+					: `${user.username} added a movie to a list`;
+			break;
+		case "created_list":
+			message = targetList
+				? `${user.username} created a new list: "${targetList.name}"`
+				: `${user.username} created a new list`;
+			break;
+		case "commented":
+			message =
+				targetMovie && comment
+					? `${user.username} commented on ${targetMovie.title}: "${comment.content}"`
+					: comment
+					? `${user.username} left a comment: "${comment.content}"`
+					: `${user.username} left a comment`;
+			break;
+		default:
+			message = `${user.username} performed an action: ${type}`;
+	}
+
+	return <div>{message}</div>;
 }
+
 export default ActivityItem;
