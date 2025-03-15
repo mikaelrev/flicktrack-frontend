4a3dc6b00ab5c5ead78d916711e5a4275c8eaf62
mikaelrev <79143791+mikaelrev@users.noreply.github.com>
2025-03-15 16:42:36 +0100

    Adds search functionality for movies page

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -11,27 +11,46 @@ function Movies() {
 	const [searchParams, setSearchParams] = useSearchParams();
 
 	const page = parseInt(searchParams.get("page")) || 1;
+	const query = searchParams.get("query") || "";
 
-	const fetchMovies = async (page) => {
-		const res = await fetch(
-			`https://api.themoviedb.org/3/movie/popular?page=${page}&api_key=${TMDB_API_KEY}`
-		);
+	const fetchMovies = async (page, query) => {
+		const endpoint = query
+			? `https://api.themoviedb.org/3/search/movie?query=${query}&page=${page}&api_key=${TMDB_API_KEY}`
+			: `https://api.themoviedb.org/3/movie/popular?page=${page}&api_key=${TMDB_API_KEY}`;
+
+		const res = await fetch(endpoint);
 		const data = await res.json();
-		setMovies(data.results);
+		console.log("Fetched movies:", data.results);
+
+		setMovies(data.results || []);
 		setTotalPages(data.total_pages > 500 ? 500 : data.total_pages);
 	};
 
 	useEffect(() => {
-		fetchMovies(page);
-	}, [page]);
+		fetchMovies(page, query);
+	}, [page, query]);
 
 	const goToPage = (newPage) => {
 		setSearchParams({ page: newPage });
 	};
 
+	const handleSearch = (event) => {
+		const newQuery = event.target.value;
+		setSearchParams({ query: newQuery, page: 1 });
+	};
+
 	return (
 		<div>
 			<h1 className="text-5xl font-extrabold text-white mb-5">Movies</h1>
+
+			<input
+				type="text"
+				placeholder="Search for a movie..."
+				value={query}
+				onChange={handleSearch}
+				className="p-2 mb-5 text-white border-1 rounded"
+			/>
+
 			<ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-5">
 				{movies.map((movie) => (
 					<MovieCard movie={movie} key={movie.id} />
